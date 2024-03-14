10bbe46bb5a50c806d553fe43fd5170fbd2fddd3
Sinethemba Nontshintshi <tushensu@gmail.com>
2024-03-14 11:01:04 +0200

    Update decrypt_credentials.sh

--- decrypt_credentials.sh
+++ decrypt_credentials.sh
@@ -1,1 +1,1 @@
-gpg --quiet --batch --yes --decrypt --passphrase= --output .df-credentials.json .df-credentials.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$DATAFORM_PASSPHRASE" --output .df-credentials.json .df-credentials.json.gpg
